a60afb43ffdefd946054448b015acfa887f2fa02
alexpaunero <103605819+alexpaunero@users.noreply.github.com>
2022-07-08 14:47:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const Calculate = {
-  
+  factorial(inputNum) {
+    if (inputNum === 0){
+      return 1;
+    } else {
+      let factorialResult = inputNum;
+      for (let i = inputNum - 1; i > 0; i--) {
+        factorialResult *= i;
+      }
+      return factorialResult;
+    }
+  }
 }
 
 module.exports = Calculate;
