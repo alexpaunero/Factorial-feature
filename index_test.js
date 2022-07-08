d71ddf75551457f523ac4ab0c3e8c96c1f3faad9
alexpaunero <103605819+alexpaunero@users.noreply.github.com>
2022-07-08 14:47:39 +0100

    Update index_test.js

--- index_test.js
+++ index_test.js
@@ -3,6 +3,16 @@ var Calculate =  require('../index.js')
 
 describe('Calculate', () => {
   describe('.factorial', () => {
+    it('The output of 5! is equal to 120', () => {
+      //Setup
+      const inputNum = 5;
+      const expected = 120;
 
+      //Exercise
+      const exercise = Calculate.factorial(inputNum);
+
+      //Verify
+      assert.equal(exercise, expected);
+    });
   });
 });
